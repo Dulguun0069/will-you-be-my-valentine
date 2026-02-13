9e30f0fb73aa25fb011d0f8a48402a7dc6532f31
Dulguun0069 <dulguunu0609@gmail.com>
2026-02-13 16:22:10 +0800

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -12,22 +12,17 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
-      ":((((",
-      "PRETTY PLEASE",
-      "Estoy muerto",
-      "No :(",
+      "Үгүй",
+      "Үнэхээр үү ?",
+      "Ингэж гоёор хэлж байхад уу ?",
+      "Өөлхнөө",
+      "Гоё шоколад өгнө шүү",
+      "Цэцэг гэвэл ямар сонсогдож байна ?",
+      "Гуйж байна",
+      "Зозо",
+      "Үхийдө",
+      "Гуйж байна шдэ ",
+      ":(((("
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -38,7 +33,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-4xl font-bold"> Хайртай шүү чамдаа үнсье   ;))</div>
         </>
       ) : (
         <>
@@ -46,20 +41,20 @@ export default function Page() {
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="my-4 text-4xl">Чи миний Валентин болох уу ?</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
               style={{ fontSize: yesButtonSize }}
               onClick={() => setYesPressed(true)}
             >
-              Yes
+              Тийм
             </button>
             <button
               onClick={handleNoClick}
               className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
             >
-              {noCount === 0 ? "No" : getNoButtonText()}
+              {noCount === 0 ? "Үгүй" : getNoButtonText()}
             </button>
           </div>
         </>
